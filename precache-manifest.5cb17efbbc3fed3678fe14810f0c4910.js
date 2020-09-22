fedc6e779e52de135acf646202492479cb3a9ffd
paperon26 <71565125+paperon26@users.noreply.github.com>
2020-09-22 15:29:13 +0800

    first commit

---
+++ precache-manifest.5cb17efbbc3fed3678fe14810f0c4910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b153a352e56e91774806aec3b7a30a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "634390529b140066964d",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "b6f85461808da93017f3",
+    "url": "/static/css/main.6c52d072.chunk.css"
+  },
+  {
+    "revision": "634390529b140066964d",
+    "url": "/static/js/2.cb22a5c8.chunk.js"
+  },
+  {
+    "revision": "11231a3b5100a082a8056a7ec6aad93d",
+    "url": "/static/js/2.cb22a5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f85461808da93017f3",
+    "url": "/static/js/main.6e7e0e86.chunk.js"
+  },
+  {
+    "revision": "95cff9b002b587359e61",
+    "url": "/static/js/runtime-main.1692d5aa.js"
+  }
+]);
